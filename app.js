d6723addff0f6ede8acdcd22e4501705e6983ab8
Jacquin Wynn <jacquinwynn@Jacquins-MacBook-Pro.local>
2016-07-11 12:15:41 -0400

    Add Recipe Form that adds New Recipes

--- app.js
+++ app.js
@@ -8,7 +8,16 @@ var pg = require('pg');
 var app = express();
 
 // DB Connection
-var connect = "postgres://wynndigital:1234567@localhost/wynnrecipebookdb";
+var config = {
+  user: 'wynndigital', //env var: PGUSER
+  database: 'wynnrecipebookdb', //env var: PGDATABASE
+  password: '1234567', //env var: PGPASSWORD
+  port: 5432, //env var: PGPORT
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+};
+
+var pool = new pg.Pool(config);
 
 // Assign Dust Engine
 app.engine('dust', consolidate.dust);
@@ -25,8 +34,33 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res){
-  res.render('index');
-})
+  pool.connect(function(err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query('SELECT * FROM recipes', function(err, result) {
+    //call `done()` to release the client back to the pool
+
+    if(err) {
+      return console.error('error running query', err);
+    }
+    res.render('index', {recipes: result.rows})
+    done();
+    //output: 1
+  });
+});
+});
+
+app.post('/add', function(req, res) {
+  pool.connect(function(err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query("INSERT INTO recipes(name, ingredients, directions) VALUES($1, $2, $3)", [req.body.name, req.body.ingredients, req.body.directions]);
+  done();
+  res.redirect('/');
+  });
+});
 
 //Server
 app.listen(3000, function(){
